1df7b5ce0d2b3c2069af4521d4691e99f555887c
Gabriel Amphoux <gabriel.amphoux@gmail.com>
2025-12-01 22:23:58 +0100

    image

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('games.leaderboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <img src="{{ asset('logotarot.png') }}" alt="Logo" class="block h-10 w-auto" />
                     </a>
                 </div>
 
